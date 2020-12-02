ca5c9c2aedb60b8d6688819db6dfd0b3b950a953
mkiezik <m.kiezik@intratel.pl>
2020-12-02 14:15:18 +0100

    Day 1

--- day1/day1.rb
+++ day1/day1.rb
@@ -8,25 +8,21 @@ def load_data
   return data
 end
 
-def find_sum_of_2()
-  array = load_data
-  array.each do |x|
-    array.each do |y|
-      if x+y == 2020
-        return x*y
-      end
+def find_sum_of_2
+  data = load_data
+  data.each do |x|
+    data.each do |y|
+      return x*y if x+y == 2020
     end
   end
 end
 
-def find_sum_of_3()
-  array = load_data
-  array.each do |x|
-    array.each do |y|
-      array.each do |z|
-        if x+y+z == 2020
-          return x*y*z
-        end
+def find_sum_of_3
+  data = load_data
+  data.each do |x|
+    data.each do |y|
+      data.each do |z|
+        return x*y*z if x+y+z == 2020
       end
     end
   end
